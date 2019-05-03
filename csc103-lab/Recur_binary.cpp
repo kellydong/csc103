88b2b3620a6cf7c0d175ee7a3c55002b87271e37
Kelly Dong <kellydong@Kellys-MacBook-Air-3.local>
2019-05-03 10:16:17 -0400

    Recursive function for binary conversion

--- csc103-lab/Recur_binary.cpp
+++ csc103-lab/Recur_binary.cpp
@@ -2,9 +2,23 @@
 using std::cin;
 using std::cout;
 
-void base2(int n){
-    if(n < 2) return n;
+/*void*/ int base2(int n){
+    if(/*n < 2*/ n==0)
+        return 0;
+        //cout << n << "\n";
     else {
-
+        return (n%2 + 10 * base2(n/2));
+        /*
+        base2(n/2);
+        base2(n%2);
+        */
     }
+}
+
+int main(){
+    int x;
+    cin >> x;
+    cout << base2(x);
+    cout << "\n";
+    return 0;
 }
